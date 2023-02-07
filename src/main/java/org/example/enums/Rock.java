322273832d2283f1d0482b8407624e87c5fd91db
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2023-02-07 06:48:43 -0300

    :sparkles: Rock

---
+++ src/main/java/org/example/enums/Rock.java
@@ -0,0 +1,31 @@
+package org.example.enums;
+
+import static org.example.enums.Outcome.*;
+
+public class Rock implements Item {
+
+    @Override
+    public Outcome compete(Item it) {
+        return it.eval(this);
+    }
+
+    @Override
+    public Outcome eval(Paper p) {
+        return WIN;
+    }
+
+    @Override
+    public Outcome eval(Scissors s) {
+        return LOSE;
+    }
+
+    @Override
+    public Outcome eval(Rock r) {
+        return DRAW;
+    }
+
+    @Override
+    public String toString() {
+        return "Rock";
+    }
+}
